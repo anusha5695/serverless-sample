13acb1ad1b976969fa0a22f300a74ac72f5d7bf0
ANUSHA S <anushas@Anushas-MacBook-Pro.local>
2021-03-22 16:51:42 +0530

    Fail

--- test/index.test.js
+++ test/index.test.js
@@ -2,6 +2,6 @@ const { transform } = require('../src/index');
 
 describe("Transform",()=> {
    test("should add transformed as true", ()=>{
-       expect(transform({a:1})).toEqual({a:1, transformed:true});
+       expect(transform({a:1})).toEqual({a:1, transformed:false});
    })
 });
